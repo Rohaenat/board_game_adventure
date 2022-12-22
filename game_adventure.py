685ede981aa3c51fcf3cad87cc1f8f5230ade908
Rohaenat <eniolamusty@gmail.com>
2022-12-22 14:32:13 +0100

    commit message

--- game_adventure.py
+++ game_adventure.py
@@ -1,32 +1,36 @@
-a_board = [(0,0),(0,1),(0,2),(0,3),(0,4),
-          (1,0),(1,1),(1,2),(1,3),(1,4),
-          (2,0),(2,1),(2,2),(2,3),(2,4),
-          (3,0),(3,1),(3,2),(3,3),(3,4),
-          (4,0),(4,1),(4,2),(4,3),(4,4)]
-
 class Board:
-    
-    def __init__(self, size, mylist):
+    def __init__(self, size):
         self.size = size
-        self.mylist = [None] * size
+
+    def theBoard(self):
+        print("-----------------------------------")
+        for i in [1,2,3,4,5,6]:
+            if i == 1:
+                i = ""
+            print("| ", end="")
+            print(i, end="")
+            print("  | ", end="")
+            for j in [0,1,2,3,4,5]:
+                if j == 1:
+                    j = ""
+                print(j, end="")
+                print("  | ", end="")
+            print("\n")
+
+            print("------------------------------------")
              
-board = Board(25, "mylist")
-board.mylist[0] = "herb"
-# print(board.mylist)
+board = Board(25)
+board.theBoard()
 
-print("-----------------------------------")
-for i in [1,2,3,4,5,6]:
-    if i == 1:
-        i = ""
-    print("| ", end="")
-    print(i, end="")
-    print("  | ", end="")
-    for j in [0,1,2,3,4,5]:
-        if j == 1:
-            j = ""
-        print(j, end="")
-        print("  | ", end="")
-    print("\n")
+class NPE(Board):
+    def __init__(self, size, herb, bread, block1, block2):
+        super().__init__(size)
+        self.herb = herb
+        self.bread = bread
+        self.block1 = block1
+        self.block2 = block2
 
-    print("------------------------------------")
-                
+    def npe_herb(self):
+            print("#")
+
+npe = NPE(25, '#', '@', '^', '^^')
